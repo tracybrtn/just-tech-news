05b3550fe7a236e13d7554b67e29267076035594
tracybrtn <tracynburton@gmail.com>
2022-10-25 15:57:12 -0500

    working  comments and upvote feature

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -80,27 +80,32 @@ router.get('/:id', (req, res) => {
 //Create a post
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.body.user_id
-  })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+ // make sure the session exists first
+ if (req.session) {
+  // pass session id along with all destructured properties on req.body
+  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
+}
 });
 
 router.put('/:id', (req, res) => {
