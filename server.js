e91825aa872d571af795a6e1b048fb1ff6d9542b
tracybrtn <tracynburton@gmail.com>
2022-10-28 11:39:57 -0500

    introduced partials and helper functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +25,7 @@ const sess = {
 app.use(session(sess));
 
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
